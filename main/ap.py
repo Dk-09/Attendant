bb4f833dbe1e24e5fdafb7f25c92d51eb0c7f7d7
Dk-09 <dhruv.k.09.02@gmail.com>
2021-09-21 14:51:52 +0530

    Edited req.txt and some tweaks

--- main/ap.py
+++ main/ap.py
@@ -49,7 +49,7 @@ while True:
             y1, x2, y2, x1 = y1*4, x2*4, y2*4, x1*4
             cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),2)
             cv2.rectangle(img,(x1,y2-35),(x2,y2),(0,255,0),cv2.FILLED)
-            cv2.putText(img,name,(x1+6,y2-6),cv2.FONT_HERSHEY_PLAIN,1,(255,255,255),2)
+            cv2.putText(img,name,(x1+6,y2-6),cv2.FONT_HERSHEY_COMPLEX_SMALL,1,(255,255,255),2)
 
     cv2.imshow("Webcam", img)
     k = cv2.waitKey(1)
